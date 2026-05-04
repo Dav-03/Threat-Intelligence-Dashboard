1dc7f441ff825e97f7c5721cfb0d0619588bab82
Dav-03 <davidcollege2005@gmail.com>
2026-05-03 20:20:15 -0700

    added 4 table models for the IoC, Feeds, Alerts, and Users

---
+++ api/app/models/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+
+class IoC(Base):
+    __tablename__ = "iocs"
+
+    id = Column(Integer, primary_key = True)
+    type = Column(String)
+    value = Column(String)
+    source = Column(String)
+    severity = Column(String)
+    timestamp = Column(TIMESTAMP)
+
+
+    alerts = relationship("Alerts", back_populates = 'ioc')
+
+#Using Shodan
+class Feeds(Base):
+    __tablename__ = "feeds"
+
+    ip = Column(String, primary_key = True)
+    port = Column(Integer)
+    org = Column(String)
+    country = Column(String)
+    city = Column(String)
+    hostname = Column(String)
+    data = Column(String)
+    timestamp = Column(TIMESTAMP)
+
+class Alerts(Base):
+    __tablename__ = "alerts"
+
+    ioc_id = Column(Integer, ForeignKey("iocs.id"))
+    id = Column(Integer, primary_key = True)
+    rule_name = Column(String)
+    description = Column(String)
+    severity = Column(String)
+    timestamp = Column(TIMESTAMP)
+
+    ioc = relationship("IoC", back_populates = "alerts")
+
+class Users(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key = True)
+    username = Column(String, unique = True)
+    hashed_password = Column(String)
+    email = Column(String)
+
+
